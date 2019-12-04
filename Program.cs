72e3affd97e1cfe231fdbca1a877383ba4ec4381
Vincent Petetin <vincent.petetin@chausson-materiaux.fr>
2019-12-04 22:29:56 +0100

    Save pr into Excel (with bug :))

--- Program.cs
+++ Program.cs
@@ -12,20 +12,11 @@ namespace PullRequetStat
 
             IEnumerable<PullRequestModel> prs = client.GetPullRequests(SearchCriterias.Completed);
 
-            IEnumerable<TimeSpan> filtered = prs
-                .Where(item => item.CreationDate >= new DateTime(2019, 11, 27))
-                .Select(item => item.Duration);
+            IEnumerable<PullRequestModel> filtered = prs
+                .Where(item => item.CreationDate >= new DateTime(2019, 11, 27));
 
-            double average = filtered.Average(item => item.TotalMilliseconds);
-            Console.WriteLine($"Average: {TimeSpan.FromMilliseconds(average)}");
-            
-            double min = filtered.Min(item => item.TotalMilliseconds);
-            Console.WriteLine($"Min: {TimeSpan.FromMilliseconds(min)}");
-            
-            double max = filtered.Max(item => item.TotalMilliseconds);
-            Console.WriteLine($"Max: {TimeSpan.FromMilliseconds(max)}");
-
-            
+            var storage = new PullRequestStorage(filtered);
+            storage.Save("pr.xlsx");
         }
     }
 }
---
+++ PullRequestStorage.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using System.IO;
+using ClosedXML.Excel;
+
+class PullRequestStorage
+{
+    protected readonly IEnumerable<PullRequestModel> model;
+
+    public PullRequestStorage(IEnumerable<PullRequestModel> model)
+    {
+        this.model = model;
+    }
+
+    protected void AddValues(IXLRow row, IEnumerable<object> values)
+    {
+        int i = 1;
+        foreach (var value in values)
+        {
+            row.Cell(i++).Value = value;
+        }
+    }
+
+    protected IEnumerable<object> ExtractValues(PullRequestModel pr)
+    {
+        yield return pr.CreationDate;
+        yield return pr.ClosedDate;
+        yield return pr.Duration;
+    }
+
+    protected void AddModel(IXLRow row)
+    {
+        foreach (var item in this.model)
+        {
+            var values = ExtractValues(item);
+            AddValues(row, values);
+        }
+    }
+
+    public void Save(string path)
+    {
+        using (var workbook = new XLWorkbook())
+        {
+            //TODO: improve worksheet title with more information than date or something else
+            IXLWorksheet ws = workbook.Worksheets.Add("Pull Request Stat");
+
+            var header = new string[] { "CreationDate", "ClosedDate", "Duration" };
+            AddValues(ws.Row(1), header);
+            AddModel(ws.Row(2));
+
+            workbook.SaveAs(path);
+        }
+    }
+}
